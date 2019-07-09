a4cbef9dfdebb9c5a7ffafdf528cadb0945f1a5f
nicballesteros <nicballesteros2@gmail.com>
2019-07-09 02:23:06 -0400

    fixing corb even more

--- client/app.js
+++ client/app.js
@@ -1,13 +1,12 @@
-const URL = "http://192.168.2.55/api/v1/todos/";
+const URL = "http://192.168.2.55:5000/api/v1/todos/";
 
 function getTodo(){
     $.ajax({
         url: URL,
         type: 'GET',
         crossDomain: true,
-        success: function(){
+        success: function(result){
             console.log(result);
-            console.log("hello");
         },
         error: function(){
             console.log("error thrown");
@@ -17,22 +16,49 @@ function getTodo(){
 
 $(document).ready(function(){ 
     getTodo();
-    
+
     $('#add').click(function(){
+        //parse data
+        
+        
+        let jsonObj = { title: "nic", description: "nic2"};
+        let jsonData = JSON.stringify(jsonObj, null, 2);
+        //jsonData = jsonData.serialize();
+        console.log(jsonData);
+        //api call
         $.ajax({
             url: URL,
             type: 'POST',
-            contentType: 'application/x-www-form-urlencoded',
-            dataType: 'application/json',
-            data: {title: nic, description: nic2},
-            success: function(){
+            crossDomain: true,
+            contentType: 'application/json',
+            data: jsonData,
+            success: (result) => {
                 console.log(result);
+                getTodo();
+            },
+            error: (err) => {
+                console.log(err);
+                console.log("error");
             }
         });
     });
     
     $('#delete').click(function(){
-       console.log("add"); 
+        let id = 2;
+        
+        
+        
+        $.ajax({
+            url: URL + id,
+            type: 'DELETE',
+            contentType: 'application/x-www-form-urlencoded',
+            success: function(result){
+                console.log(result);
+            },
+            error: (err) => {
+                console.log(err);            
+            }
+        });
     });
     $('#update').click(function(){
        console.log("add"); 
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const server = express();
 server.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT,DELETE,GET,POST");
     next();
 })
 
